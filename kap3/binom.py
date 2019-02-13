c8f70fa461fbdce5add60172671133d8872b67e1
Balthasar Reuter <balthasar.reuter@fau.de>
2019-02-13 17:50:09 +0100

    Copyright header added to files

--- kap3/binom.py
+++ kap3/binom.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python3
 
+# This file belongs to the collection of Python codes from the book
+#
+#   "Mit Mathe richtig anfangen - Eine Einfuehrung mit integrierter Anwendung
+#    der Programmiersprache Python"
+#
+# by Peter Knabner, Balthasar Reuter, and Raphael Schulz.
+# Published by Springer-Spektrum, 2019.
+#
+# If you want to use this code please include a reference to this publication.
+#
+# Copyright (C) 2019 Peter Knabner, Balthasar Reuter, Raphael Schulz.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 
 def nchoosek(n, k):
     """Berechnet den Binomialkoeffizienten "n ueber k",
